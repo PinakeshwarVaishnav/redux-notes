19c45a3619785221a7b0db6efef739f5b152359c
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-10-29 12:53:42 +0530

    application code for part6-2

--- redux-notes/src/components/Notes.jsx
+++ redux-notes/src/components/Notes.jsx
@@ -2,9 +2,9 @@ import { useDispatch, useSelector } from 'react-redux'
 import { toggleImportanceOf } from '../reducers/noteReducer'
 
 const Note = ({ note, handleClick }) => {
-  return(
+  return (
     <li onClick={handleClick}>
-      {note.content} 
+      {note.content}
       <strong> {note.important ? 'important' : ''}</strong>
     </li>
   )
@@ -12,15 +12,22 @@ const Note = ({ note, handleClick }) => {
 
 const Notes = () => {
   const dispatch = useDispatch()
-  const notes = useSelector(state => state)
+  const notes = useSelector(state => {
+    if (state.filter === 'ALL') {
+      return state.notes
+    }
+    return state.filter === 'IMPORTANT'
+      ? state.notes.filter(note => note.important)
+      : state.notes.filter(note => !note.important)
+  })
 
-  return(
+  return (
     <ul>
       {notes.map(note =>
         <Note
           key={note.id}
           note={note}
-          handleClick={() => 
+          handleClick={() =>
             dispatch(toggleImportanceOf(note.id))
           }
         />
