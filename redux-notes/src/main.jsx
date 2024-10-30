d6e9dfeb43ecf92773f08c5f754ad6441f6078e0
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-10-30 18:36:39 +0530

    application code for part6-3

--- redux-notes/src/main.jsx
+++ redux-notes/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
-import noteReducer from './reducers/noteReducer'
+import noteReducer, { setNotes } from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
 const store = configureStore({
@@ -12,6 +12,9 @@ const store = configureStore({
     filter: filterReducer
   }
 })
+
+noteService.getAll().then(notes =>
+  store.dispatch(setNotes(notes)))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
